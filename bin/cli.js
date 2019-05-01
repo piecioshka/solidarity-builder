4cdd866a95121e4730c324d221ce13da6a1293df
Piotr Kowalski <piecioshka@gmail.com>
2019-05-02 00:41:12 +0200

    Ask where output file should generated

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const { prompt } = require('enquirer');
 const colors = require('colors');
+const resolve = require('resolve-dir');
 
 const { printJSON, loadJSON, saveJSON } = require('../src/helpers/json-helper');
 const { listDirectory, createDirectory } = require('../src/helpers/directory-helper');
@@ -12,22 +13,28 @@ const { ignoreCase } = require('../src/helpers/sort-helper');
 const ROOT_PATH = path.join(__dirname, '..');
 const MODULES_PATH = path.join(ROOT_PATH, 'modules');
 const SOURCE_FILE_PATH = path.join(ROOT_PATH, 'src', '.solidarity.example.json');
-const DIST_DIR_PATH = path.join(ROOT_PATH, 'dist');
-const DIST_FILE_PATH = path.join(DIST_DIR_PATH, `.solidarity.json`);
+const OUTPUT_FILENAME = '.solidarity.json';
 
-const NAME = 'modules';
+const QUE_DIST = 'dist_dir_path';
+const QUE_NAME = 'modules';
 
 const question = [
     {
+        type: 'input',
+        name: QUE_DIST,
+        message: `Directory where config file will be created`,
+        initial: '.'
+    },
+    {
         type: 'multiselect',
-        name: NAME,
+        name: QUE_NAME,
         message: 'Choose modules:',
         choices: []
     }
 ];
 
-function getModules(answers) {
-    return answers[NAME]
+function getModules(selectedModules) {
+    return selectedModules
         .map(m => {
             return {
                 [m]: loadJSON(
@@ -49,17 +56,43 @@ async function main() {
 
     // printJSON(modules);
 
-    question[0].choices.push(...modules);
+    question.find((q) => q.name === QUE_NAME).choices.push(...modules);
 
     const answers = await prompt(question);
-    const requirements = getModules(answers);
+
+    const selectedModules = answers[QUE_NAME];
+    if (selectedModules.length === 0) {
+        console.error('Error: Please select at least one module'.red);
+        process.exit(1);
+    }
+
+    const distDirectory = answers[QUE_DIST].trim();
+    let distFilePath = OUTPUT_FILENAME;
+
+    if (distDirectory) {
+        const normalizeDistDirectory = path.resolve(resolve(distDirectory));
+        await createDirectory(normalizeDistDirectory);
+        distFilePath = normalizeDistDirectory + path.sep + OUTPUT_FILENAME
+    }
+
+    const requirements = getModules(selectedModules);
 
     const configFile = loadJSON(SOURCE_FILE_PATH);
     Object.assign(configFile, { requirements });
 
-    await createDirectory(DIST_DIR_PATH);
-    await saveJSON(DIST_FILE_PATH, configFile);
-    console.log(`${colors.green('✔')} File with selected modules was created: ${DIST_FILE_PATH}`);
+    await saveJSON(distFilePath, configFile);
+
+    const message = 'Create config file at';
+    console.log(
+        `${colors.yellow('✔')} ${colors.bold(message)}: ${distFilePath}`
+    );
 }
 
-main().catch(err => console.error(err));
+main().catch(err => {
+    const message = (typeof err === 'object')
+        ? err.message
+        : 'Unknown';
+    console.error(
+        colors.red(`Error: ${message}`)
+    )
+});
